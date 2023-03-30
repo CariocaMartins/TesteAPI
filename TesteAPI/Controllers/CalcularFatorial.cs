b5c1fa5f21901d43a9aed39c73f8b17bf63136e3
natanmartins0908@gmail.com <natanmartins0908@gmail.com>
2023-03-30 19:28:31 -0300

    commit

--- TesteAPI/Controllers/CalcularFatorial.cs
+++ TesteAPI/Controllers/CalcularFatorial.cs
@@ -9,19 +9,25 @@ namespace api.Controllers
     {
         [Route("/")]
         [HttpGet]
-        public string Calcular(int number)
-        {
-            int fatorial = 1;
-            string conta = "1";
+public string Calcular([FromQuery] int number)
+{
+    if (number < 0)
+    {
+        return "O número deve ser maior ou igual a zero";
+    }
+    
+    int fatorial = 1;
+    string conta = "1";
 
-            for (int i = 2; i <= number; i++)
-            {
-                fatorial *= i;
-                conta += " * " + i;
-            }
+    for (int i = 2; i <= number; i++)
+    {
+        fatorial *= i;
+        conta += " * " + i;
+    }
 
-            return $"{conta} = {fatorial}";
-        }
+    return $"{conta} = {fatorial}";
+}
+
 
     }
 
